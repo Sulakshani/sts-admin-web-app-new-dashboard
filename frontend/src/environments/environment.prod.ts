dd47577a0c2634b4b2cd07da19ffb1c4a1cae29a
Sulakshani Dissanayake <sulakshanidissanayake@gmail.com>
2026-06-07 10:36:03 +0530

    update the backend choreo link in the frontend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://6cfadfe7-c9f0-4586-8b75-91503c8f02d6-dev.e1-us-east-azure.choreoapis.dev/admin-dashboard/backend/v1.0/api/v1'
+  apiUrl: 'https://df2c3a4b-2991-4c50-816e-4f1d0896f5d9-dev.e1-us-east-azure.choreoapis.dev/aasl/backend/v1.0/api'
 };
