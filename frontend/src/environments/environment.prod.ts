db44cd078766ff79cb9a2662c02f45d1423d7c91
Sulakshani Dissanayake <sulakshanidissanayake@gmail.com>
2026-06-08 10:13:32 +0530

    Update the openapi.yaml file with oauth configuration

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://df2c3a4b-2991-4c50-816e-4f1d0896f5d9-dev.e1-us-east-azure.choreoapis.dev/aasl/backend/v1.0/api'
+  apiUrl: 'https://df2c3a4b-2991-4c50-816e-4f1d0896f5d9-prod.e1-us-east-azure.choreoapis.dev/groundlog/backend/v1.0/api'
 };
